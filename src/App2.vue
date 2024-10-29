458c9f4bcfef3562ea9ed31891157d6ee04b6c33
Azagal258 <azagal.minecraft@gmail.com>
2024-10-29 19:01:52 +0100

    Branch initial commit / Trying display a component

---
+++ src/App2.vue
@@ -0,0 +1,11 @@
+<script lang="js">
+import ObjektDisplay from './components/Objekt-display.vue';
+
+</script>
+
+<template>
+    <div>
+        <h1>This is a title</h1>
+        <ObjektDisplay />
+    </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import App2 from './App2.vue'
 
-createApp(App).mount('#app')
+const appComponent = App2 // testing stuff, I have no idea of what I'm doing
+
+createApp(appComponent).mount('#app')
