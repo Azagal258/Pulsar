5d09c5493d1f944a79098339c11f65d6fd55f692
Azagal258 <azagal.minecraft@gmail.com>
2024-10-31 02:54:57 +0100

    Move code over to adapted files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import App2 from './App2.vue'
 
-const appComponent = App2 // testing stuff, I have no idea of what I'm doing
-
-createApp(appComponent).mount('#app')
+createApp(App).mount('#app')
