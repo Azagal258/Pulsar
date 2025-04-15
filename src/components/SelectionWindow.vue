0f11691d632cf1a49f5eb183c9b1a1bfdf67fe7e
Azagal258 <azagal.minecraft@gmail.com>
2025-04-16 01:28:40 +0200

    Transfer over and tweak styles

--- src/components/SelectionWindow.vue
+++ src/components/SelectionWindow.vue
@@ -65,8 +65,85 @@ const downloadImagesAsZip = async() => {
 
 #modal-window-attempt {
     position: relative;
-    border: 5px solid green;
+    border: 2px solid grey;
     background-color: #242424;
     padding: 5px;
+    border-radius: 10px;
 }
+
+#objekt-list {
+    margin-bottom: 0.5rem;
+}
+
+#objekt-list .list-header {
+    margin-bottom: 0.3rem;
+}
+
+#download-button {
+    margin-bottom: 0.5rem;
+}
+
+.selected-objekt-line {
+    margin-bottom: 0.2rem;
+}
+
+.selected-objekt-line #objekt-id {
+    margin-left: 1.5rem;
+}
+
+.button-wrapper {
+    position: relative;
+
+    cursor: pointer;
+}
+
+.button-wrapper .button {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.button-wrapper .checkmark{
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: .9rem;
+    width: .9rem;
+    border: 2px solid #9c9c9c;
+    border-radius: 5px;
+    background-color: #242424;
+    transition-duration: 0.5s;
+    
+}
+
+.button-wrapper .checkmark:hover{
+    background-color: rgb(60, 60, 60);
+}
+
+.button-wrapper input:checked~ .checkmark{
+    background-color: #2196F3;
+    border: 2px solid #2196F3;
+}
+
+.checkmark:after{
+    content: "";
+    position: absolute;
+    display: none;
+}
+
+.button-wrapper input:checked ~ .checkmark:after {
+  display: block;
+}
+
+.button-wrapper .checkmark::after{
+    left: 4px;
+    top: 0;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-radius: 2px;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+}
+
 </style>
